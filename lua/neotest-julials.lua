f0331cb0cfe954ac898038596d8817054fc22083
Sam Buercklin <sam.buercklin@gmail.com>
2025-01-29 15:41:56 -0500

    added basic types for handling julia test results

--- lua/neotest-julials.lua
+++ lua/neotest-julials.lua
@@ -1,6 +1,22 @@
 local JuliaAdapter = { name = "neotest-julia" }
 
+---@type { [string]: TestItemDetailLSP }
 local julia_tests = {}
+
+---@type { [string]: TestItemDetailLSP }
+local julia_test_setups = {}
+
+---@type { [string]: TestErrorDetailLSP }
+local julia_test_errors = {}
+
+local julia_environment = ""
+local logging = false
+
+local log = function(...)
+    if logging then
+        vim.print("NEOTEST-JULIALS:", ...)
+    end
+end
 
 JuliaAdapter.root = function(dir)
     local f = vim.fs.dirname(vim.fs.find('Project.toml', { upward = true, path = dir })[1])
@@ -70,32 +86,73 @@ JuliaAdapter.discover_positions = function(file_path)
     return require('neotest.types.tree').from_list(positions, keyfunc)
 end
 
+---Handles the julia test results from julia/publishTests, populating the vim.g.tests_jl
+---global variable with information about the tests
+---@param err any
+---@param result PublishTestsParams
+---@param ctx any
+---@param config any
+---@return boolean
 local handle_julia_tests = function(err, result, ctx, config)
     local uri = result.uri
-    local new_table = {}
+    local new_test_table = {}
+    local new_setup_table = {}
+    local new_error_table = {}
 
+    log("Handling tests")
+    log(result)
     if result.testItemDetails ~= nil then
         for _, test in pairs(result.testItemDetails) do
-            table.insert(new_table, test)
+            table.insert(new_test_table, test)
+        end
+    end
+    if result.testSetupDetails ~= nil then
+        for _, setup in pairs(result.testSetupDetails) do
+            table.insert(new_setup_table, setup)
+        end
+    end
+    if result.testErrorDetails ~= nil then
+        for _, error in pairs(result.testErrorDetails) do
+            table.insert(new_error_table, error)
         end
     end
 
-    local updated_table = julia_tests
-    updated_table[uri] = new_table
-    julia_tests = updated_table
+    local updated_test_table = julia_tests
+    updated_test_table[uri] = new_test_table
+    julia_tests = updated_test_table
     vim.g.tests_jl = julia_tests
+
+    local updated_setup_table = julia_test_setups
+    updated_setup_table[uri] = new_setup_table
+    julia_test_setups = updated_setup_table
+    vim.g.setups_jl = julia_test_setups
+
+    local updated_error_table = julia_test_errors
+    updated_error_table[uri] = new_error_table
+    julia_test_errors = updated_error_table
+    vim.g.errors_jl = julia_test_errors
+
+    log("Tests", julia_tests)
+    log("Setups", julia_test_setups)
+    log("Errors", julia_test_errors)
 
     return true
 end
 
 local default_options = {
     activate = true,
+    environment = "@test_item_controller",
+    logging = false
 }
 
 JuliaAdapter.setup = function(_opts)
     local opts = vim.tbl_deep_extend('force', default_options, _opts)
     if opts.activate then
         vim.lsp.handlers['julia/publishTests'] = handle_julia_tests
+        julia_environment = opts.environment
+        logging = opts.logging
+
+        log("Initialized with environemt", julia_environment)
     end
 
     return true
---
+++ lua/types.lua
@@ -0,0 +1,165 @@
+---@meta
+
+-- Types below are according to LanguageServer.jl, not TestItemController.jl
+
+---@class PositionLSP
+---@field line integer
+---@field character integer
+Position = {}
+
+---@class RangeLSP
+---@field start PositionLSP
+---@field stop PositionLSP
+Range = {}
+
+---@class TestItemDetailLSP
+---@field id string
+---@field label string
+---@field range RangeLSP
+---@field code string
+---@field codeRange RangeLSP
+---@field optionDefaultImports boolean
+---@field optionTags string[]
+---@field optionSetup string[]
+TestItemDetailLSP = {}
+
+---@class TestSetupDetailLSP
+---@field name string
+---@field kind string
+---@field range RangeLSP
+---@field code string
+---@field codeRange RangeLSP
+TestSetupDetailLSP = {}
+
+---@class TestErrorDetailLSP
+---@field id string
+---@field label string
+---@field range RangeLSP
+---@field error string
+TestErrorDetailLSP = {}
+
+---@class PublishTestsParams
+---@field uri string
+---@field version integer?
+---@field testItemDetails TestItemDetailLSP[]
+---@field testSetupDetails TestSetupDetailLSP[]
+---@field testErrorDetails TestErrorDetailLSP[]
+PublishTestsParams = {}
+
+
+---@class GetTestEnvRequestParamsLSP
+---@field uri string
+GetTestEnvRequestParamsLSP = {}
+
+---@class GetTestEnvRequestParamsReturnLSP
+---@field packageName string?
+---@field packageUri string?
+---@field projectUri string?
+---@field envContentHash integer?
+GetTestEnvRequestParamsReturnLSP = {}
+
+
+
+
+
+--[[
+        Types below are according to TestItemController.jl
+]]
+
+
+--[[
+        CreateTestRunRequest types for createTestRun request
+]]
+
+---@class CreateTestRunRequestParams
+---@field testRunId string
+---@field testProfiles TestProfileTIC[]
+---@field testItems TestItemTIC[]
+---@field testSetups TestSetupTIC[]
+CreateTestRunRequestParams = {}
+
+---@class TestProfileTIC
+---@field id string
+---@field label string
+---@field juliaCmd string
+---@field juliaArgs string[]
+---@field juliaNumThreads string
+---@field juliaEnv any
+---@field maxProcessCount number
+---@field mode string
+---@field coverageRootUris string[]?
+TestProfileTIC = {}
+
+---@class TestItemTIC
+---@field id string
+---@field uri string
+---@field label string
+---@field packageName string?
+---@field packageUri string?
+---@field projectUri string?
+---@field envContentHash number?
+---@field useDefaultUsings boolean
+---@field testSetups string[]
+---@field line number
+---@field column number
+---@field code string
+---@field codeLine number
+---@field codeColumn number
+TestItemTIC = {}
+
+---@class TestSetupTIC
+---@field packageUri string?
+---@field name string
+---@field kind string
+---@field uri string
+---@field line number
+---@field column number
+---@field code string
+TestSetupTIC = {}
+
+
+
+
+
+--[[
+        CreateTestRunRequest response
+]]
+
+---@class FileCoverage
+---@field uri string
+---@field coverage number?[]
+FileCoverage = {}
+
+---@class CoverageResult
+---@field status string
+---@field coverage FileCoverage[]?
+CoverageResult = {}
+
+
+
+
+
+--[[
+        TerminateTestProcess params
+]]
+
+---@class TerminateTestProcessParams
+---@field processId string
+
+
+
+
+
+--[[
+    Notifications from the server
+]]
+
+
+--[[
+    testItemStarted notification
+]]
+
+---@class TestItemStarted
+---@field testRunId string
+---@field testItemId string
+TestItemStarted = {}
